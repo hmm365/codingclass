8744b6b6cc827c74ec98d11c078801a0692c4815
hmm365 <happyham52@gmail.com>
2022-11-01 19:59:15 +0900

    1101

--- javascript/assets/js/memory.js
+++ javascript/assets/js/memory.js
@@ -15,7 +15,7 @@ let soundMatch = new Audio([sound[0]]);
 let soundUnMatch = new Audio([sound[1]]);
 let soundSuccess = new Audio([sound[2]]);
 let timeresult = -4;
-
+let memoriScore = 100;
 let interval;
 
 // 카드 뒤집기
@@ -77,29 +77,36 @@ function shuffledCard(params) {
 	cardOne = cardTwo = "";
 	disableDeck = true;
 	matchedCard = 0;
-	timeresult = -4;
+	timeresult = -5;
 	let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
 	let result = arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
 
 	memoryCards.forEach((card, index) => {
 		card.classList.remove("flip");
-		setTimeout(() => {
-			card.classList.add("flip");
-		}, 200 * index);
-
-		setTimeout(() => {
-			card.classList.remove("flip");
-			disableDeck = false;
-		}, 4000);
-
-		let imgTag = card.querySelector(".back img");
-		imgTag.src = `../assets/img/gameimage/img-${arr[index]}.svg`;
-
-		card.addEventListener("click", flipCard);
 	});
+	setTimeout(() => {
+		memoryCards.forEach((card, index) => {
+			setTimeout(() => {
+				card.classList.add("flip");
+			}, 200 * index);
+
+			setTimeout(() => {
+				card.classList.remove("flip");
+				disableDeck = false;
+			}, 4000);
+
+			let imgTag = card.querySelector(".back img");
+			imgTag.src = `../assets/img/gameimage/img-${arr[index]}.svg`;
+
+			card.addEventListener("click", flipCard);
+		});
+	}, 1000);
 	interval = setInterval(() => {
 		timeresult++;
 		console.log(timeresult);
+		if (timeresult > 10) {
+			memoriScore--;
+		}
 	}, 1000);
 }
 
@@ -107,28 +114,29 @@ function textResult() {
 	let currentMin = Math.floor(timeresult / 60);
 	let currentSec = Math.floor(timeresult % 60);
 	let reultTime = timeresult;
+
 	if (currentMin > 0) {
 		document.querySelector(".memory__time").innerHTML = `총 <em>${currentMin}</em> 분 <em>${currentSec}</em> 초가 걸렸습니다.`;
 	} else document.querySelector(".memory__time").innerHTML = `총 <em>${currentSec}</em> 초가 걸렸습니다.`;
-	console.log(reultTime);
-	if (reultTime <= 20) {
-		document.querySelector(".memory__grade").innerText = "S";
-		document.querySelector(".memory__rank").innerHTML = "상위 <em>1</em>% 프로 입니다</span>";
-	} else if (20 < reultTime && reultTime <= 30) {
-		document.querySelector(".memory__grade").innerText = "A";
-		document.querySelector(".memory__rank").innerHTML = "상위 <em>10</em>% 프로 입니다</span>";
-	} else if (30 < reultTime && reultTime <= 50) {
-		document.querySelector(".memory__grade").innerText = "B";
-		document.querySelector(".memory__rank").innerHTML = "상위 <em>30</em>% 프로 입니다</span>";
-	} else if (50 < reultTime && reultTime <= 70) {
-		document.querySelector(".memory__grade").innerText = "C";
-		document.querySelector(".memory__rank").innerHTML = "상위 <em>60</em>% 프로 입니다</span>";
-	} else if (70 < reultTime && reultTime <= 100) {
-		document.querySelector(".memory__grade").innerText = "D";
-		document.querySelector(".memory__rank").innerHTML = "상위 <em>80</em>% 프로 입니다</span>";
+
+	if (memoriScore >= 90) {
+		document.querySelector(".memory__grade").innerText = memoriScore;
+		document.querySelector(".memory__rank").innerHTML = "당신은 <em>S</em> 등급 입니다</span>";
+	} else if (80 <= memoriScore && memoriScore < 90) {
+		document.querySelector(".memory__grade").innerText = memoriScore;
+		document.querySelector(".memory__rank").innerHTML = "당신은 <em>A</em> 등급 입니다</span>";
+	} else if (70 <= memoriScore && memoriScore < 80) {
+		document.querySelector(".memory__grade").innerText = memoriScore;
+		document.querySelector(".memory__rank").innerHTML = "당신은 <em>B</em> 등급 입니다</span>";
+	} else if (60 <= memoriScore && memoriScore < 70) {
+		document.querySelector(".memory__grade").innerText = memoriScore;
+		document.querySelector(".memory__rank").innerHTML = "당신은 <em>C</em> 등급 입니다</span>";
+	} else if (50 <= memoriScore && memoriScore < 60) {
+		document.querySelector(".memory__grade").innerText = memoriScore;
+		document.querySelector(".memory__rank").innerHTML = "당신은 <em>D</em> 등급 입니다</span>";
 	} else {
-		document.querySelector(".memory__grade").innerText = "F";
-		document.querySelector(".memory__rank").innerHTML = "상위 <em>90</em>% 프로 입니다</span>";
+		document.querySelector(".memory__grade").innerText = memoriScore;
+		document.querySelector(".memory__rank").innerHTML = "당신은 <em>F</em> 등급 입니다</span>";
 	}
 }
 
@@ -139,7 +147,9 @@ function textResult() {
 
 document.querySelector(".memory__replay").addEventListener("click", () => {
 	memoryResult.classList.remove("open");
-	shuffledCard();
+	setTimeout(() => {
+		shuffledCard();
+	}, 700);
 });
 
 document.querySelector(".memory__bye").addEventListener("click", () => {
